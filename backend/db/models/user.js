3a2aa15b5b70a108adc3ba9704b9e150eca50c62
Nicolas Borromeo <nicoborrom@gmail.com>
2024-05-28 14:19:44 -0700

    Add User model scope for user security

---
+++ backend/db/models/user.js
@@ -0,0 +1,61 @@
+'use strict';
+const {
+  Model, Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        // isNotEmail(value) {
+        //   if (value.includes('@')) {
+        //     throw new Error('username cannot be an email')
+        //   };
+        // }
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) {
+            throw new Error('username cannot be an email')
+          }
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword', 'updatedAt', 'email', 'createdAt']
+      }
+    }
+  });
+  return User;
+};
