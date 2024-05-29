0a12c0cbfe8e299bbec85e3344790e3f4d8285e3
Nicolas Borromeo <nicoborrom@gmail.com>
2024-05-29 16:27:39 -0700

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,39 +1,22 @@
 // setting API routers to hablde API requests. REST API server functionality
 
 const router = require('express').Router();
+const sessionRouter = require('./session.js')
+const userRouter = require('./users.js')
+const { restoreUser } = require('../../utils/auth.js')
+
+
+
+router.use(restoreUser)
+
+router.use('/session', sessionRouter)
+// router.use('/users', userRouter)
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
 
 
-//TESTING THE TOKEN COOKIE CREATION
-
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-
-// router.get('/set-token-cookie', async (_req, res) => {
-//     const user = await User.findOne({
-//         where: {
-//             username: 'Demo-lition'
-//         }
-//     });
-//     setTokenCookie(res, user); //this function creates the payload, creates the token and sets it in the response as a cookie
-
-//     return res.json({ user: user })
-// })
-
-const { restoreUser } = require('../../utils/auth.js')
-router.use(restoreUser)
-
-// router.get('/restore-user', (req, res) => {
-//     return res.json(req.user)
-// });
-
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get('/require-auth', requireAuth, (req, res) => {
-//     return res.json(req.user);
-// });
 
 
 module.exports = router
