a76180bc6eb342b95056ffc01afa112f6d2e8b3d
Nicolas Borromeo <nicoborrom@gmail.com>
2024-06-14 15:56:59 -0700

    commented out stack at error formater

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-// router.get('/hello/world', (req,res) => {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.send('Hello World');
-// });
-
-//Add a XSRF-TOKEN cookie to allow any developer to re-set the CSRF token cookie XSRF-TOKEN
-
-
 router.get('/api/csrf/restore', (req, res) => {
     //set a cookie on the response with the name of XSRF-TOKEN to the valuie of the req.csrfToken method's return
     const csrfToken = req.csrfToken();
