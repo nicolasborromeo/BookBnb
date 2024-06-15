b7e57154f1df698b23284b2ab78ef4d1d0954fb2
Nicolas Borromeo <nicoborrom@gmail.com>
2024-06-15 13:01:27 -0700

    new booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,19 +1,22 @@
 const router = require('express').Router();
 
-const { Spot, Booking }= require('../../db/models')
+const { Spot, Booking, SpotImage }= require('../../db/models')
 const { requireAuth, restoreUser } = require('../../utils/auth')
 
 router.get('/current', requireAuth, async (req, res, next) => {
     let userId = req.user.id
-
     let userBookings = await Booking.findAll({
         where: {userId: userId},
         include: [{
-            
+            model: Spot,
+            attributes: {exclude: ['createdAt', 'updatedAt']},
+            include: {
+                model: SpotImage
+            }
         }]
     })
 
-    res.status(200).json({bookings: bookingsRes})
+    res.status(200).json({bookings: userBookings})
 })
 
 module.exports = router
