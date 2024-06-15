0f81dfeb068f788d562c37278f00acfd0edf3248
Nicolas Borromeo <nicoborrom@gmail.com>
2024-06-14 21:54:51 -0700

    options in migration:

--- backend/db/migrations/20240615042206-create-spot-image.js
+++ backend/db/migrations/20240615042206-create-spot-image.js
@@ -5,7 +5,7 @@ let options = {}
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA
 };
-options.tableName = 'SpotImages'
+
 
 
 /** @type {import('sequelize-cli').Migration} */
@@ -44,7 +44,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'SpotImages'
