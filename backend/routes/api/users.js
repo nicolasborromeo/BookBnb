7b41ddc1ee9c98adc506d3cf131c7dd59612515d
Nicolas Borromeo <nicoborrom@gmail.com>
2024-06-15 18:59:20 -0700

    fix .2

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -21,10 +21,10 @@ const validateSignup = [
       .not()
       .isEmail()
       .withMessage('Username cannot be an email.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
+    // check('password')
+    //   .exists({ checkFalsy: true })
+    //   .isLength({ min: 6 })
+    //   .withMessage('Password must be 6 characters or more.'),
     check('firstName')
       .exists()
       .notEmpty()
